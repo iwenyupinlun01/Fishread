e8df17b2a2a0bb079b6b2e2ce4a3b5aef7e7538b
wangjungang1994 <1499370499@qq.com>
2017-06-02 18:53:23 +0800

    修改需要

--- Fishread/TopViewController.m
+++ Fishread/TopViewController.m
@@ -8,8 +8,8 @@
 
 #import "TopViewController.h"
 #import "homeViewController.h"
-#import "bookViewController.h"
 #import "infoViewController.h"
+#import "MainViewController.h"
 @interface TopViewController ()
 
 @end
@@ -25,16 +25,15 @@
 }
 
 -(void)makeVC
-    {
-        homeViewController *frist = [[homeViewController alloc]init];
-        [self setupChildViewController:frist title:@"首页" imageName:@"未点击首页" selectedImageName:@"首页-拷贝"];
-        bookViewController *second = [[bookViewController alloc]init];
-        [self setupChildViewController:second title:@"书圈" imageName:@"" selectedImageName:@""];
+{
+    homeViewController *frist = [[homeViewController alloc]init];
+    [self setupChildViewController:frist title:@"首页" imageName:@"未点击首页" selectedImageName:@"首页-拷贝"];
+    MainViewController *second = [[MainViewController alloc]init];
+    [self setupChildViewController:second title:@"书圈" imageName:@"2" selectedImageName:@"书圈"];
         
-        infoViewController *thrid = [[infoViewController alloc]init];
-        [self setupChildViewController:thrid title:@"个人中心" imageName:@"" selectedImageName:@""];
-       
-    }
+    infoViewController *thrid = [[infoViewController alloc]init];
+    [self setupChildViewController:thrid title:@"个人中心" imageName:@"3" selectedImageName:@"4"];
+}
     
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -55,12 +54,12 @@
         //    nav.navigationBarHidden = YES;
         nav.navigationBar.barStyle = UIBarStyleDefault;
         
-        childVc.navigationController.navigationBar.barTintColor=[UIColor colorWithRed:0.299 green:0.988 blue:0.970 alpha:1.000];
+        childVc.navigationController.navigationBar.barTintColor=[UIColor whiteColor];
         UIView *bgView = [[UIView alloc] initWithFrame:self.tabBar.bounds];
         bgView.backgroundColor = [UIColor whiteColor];
         [self.tabBar insertSubview:bgView atIndex:0];
         self.tabBar.opaque = YES;
-        self.tabBar.tintColor = [UIColor greenColor];
+        self.tabBar.tintColor = [UIColor wjColorFloat:@"54d48a"];
         [self addChildViewController:nav];
     }
     
