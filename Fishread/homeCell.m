e2114d9e5fecf0c61c7f4396f71b29de7208db28
wangjungang1994 <1499370499@qq.com>
2017-06-03 16:00:14 +0800

    修改首页

---
+++ Fishread/homeCell.m
@@ -0,0 +1,32 @@
+//
+//  homeCell.m
+//  Fishread
+//
+//  Created by 王俊钢 on 2017/6/3.
+//  Copyright © 2017年 wangjungang. All rights reserved.
+//
+
+#import "homeCell.h"
+
+@implementation homeCell
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+    }
+    return self;
+}
+
+-(void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+}
+
+#pragma mark - getters
+
+
+
+@end
--- Fishread/homeViewController.m
+++ Fishread/homeViewController.m
@@ -7,17 +7,29 @@
 //
 
 #import "homeViewController.h"
+#import "homeCell.h"
+#import "MyHeaderView.h"
+@interface homeViewController ()<UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout,UITextFieldDelegate>
 
-@interface homeViewController ()
+@property(strong,nonatomic)UICollectionView *myCollectionV;
+
+
 
 @end
+//设置标识
+static NSString *indentify = @"indentify";
 
 @implementation homeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.view.backgroundColor = [UIColor lightGrayColor];
+    self.navigationItem.title = @"推荐";
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    
+    [self addTheCollectionView];
+    //[self.view addSubview:self.searchtext];
     
 }
 
@@ -37,4 +49,92 @@
     [super viewDidAppear:animated];
     
 }
+
+#pragma mark - getters
+
+//创建视图
+-(void)addTheCollectionView{
+    
+    //=======================1===========================
+    //创建一个块状表格布局对象
+    UICollectionViewFlowLayout *flowL = [[UICollectionViewFlowLayout alloc] init];
+    
+    //格子的大小 (长，高)
+    flowL.itemSize = CGSizeMake(100, 130);
+    //横向最小距离
+    flowL.minimumInteritemSpacing = 1.f;
+    //    flowL.minimumLineSpacing=60.f;//代表的是纵向的空间间隔
+    //设置，上／左／下／右 边距 空间间隔数是多少
+    flowL.sectionInset = UIEdgeInsetsMake(10, 0, 0, 0);
+    //如果有多个 区 就可以拉动
+   // [flowL setScrollDirection:UICollectionViewScrollDirectionVertical];
+    
+    
+    //=======================2===========================
+    //创建一个UICollectionView
+    _myCollectionV = [[UICollectionView alloc]initWithFrame:CGRectMake(0, 0, DEVICE_WIDTH, DEVICE_HEIGHT) collectionViewLayout:flowL];
+    //设置代理为当前控制器
+    _myCollectionV.delegate = self;
+    _myCollectionV.dataSource = self;
+    //设置背景
+    _myCollectionV.backgroundColor =[UIColor whiteColor];
+    
+#pragma mark -- 注册单元格
+    //设置头部并给定大小
+    [flowL setHeaderReferenceSize:CGSizeMake(_myCollectionV.frame.size.width, 50)];
+
+    [_myCollectionV registerClass:[homeCell class] forCellWithReuseIdentifier:indentify];
+#pragma mark -- 注册头部视图
+    [_myCollectionV registerClass:[MyHeaderView class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"HeaderView"];
+    //添加视图
+    [self.view addSubview:_myCollectionV];
+    
+}
+
+#pragma mark --UICollectionView dataSource
+//有多少个Section
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+//每个section有多少个元素
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 29;
+}
+
+//每个单元格的数据
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    //初始化每个单元格
+    homeCell *cell = (homeCell *)[collectionView dequeueReusableCellWithReuseIdentifier:indentify forIndexPath:indexPath];
+    
+    //给单元格上的元素赋值
+    cell.backgroundColor = [UIColor redColor];
+    return cell;
+    
+}
+
+//设置头尾部内容
+-(UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionReusableView *reusableView = nil;
+    
+    if (kind == UICollectionElementKindSectionHeader) {
+        //定制头部视图的内容
+        MyHeaderView *headerV = (MyHeaderView *)[collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"HeaderView" forIndexPath:indexPath];
+        
+        reusableView = headerV;
+    }
+    return reusableView;
+}
+//点击单元格
+-(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"%ld",indexPath.row);
+}
+
+
+
 @end
